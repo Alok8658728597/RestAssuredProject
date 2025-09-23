18a7c96ff23f11f70628cf73287bf6c4613f5fcb
Alokswain <alokdhana123@gmail.com>
2025-09-23 18:40:30 +0530

    Added test cases for Serialization and ,Deserialization using lombok library

---
+++ src/main/java/com/restassured/pojo/PayloadUsingLombok.java
@@ -0,0 +1,13 @@
+package com.restassured.pojo;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class PayloadUsingLombok{
+	private String id;
+	private String name;
+	private DataUsingLombok data;
+	
+	
+	
+}
